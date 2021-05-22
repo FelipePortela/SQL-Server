1f443d6b4f25239e3cee4f8a70a209d6225b882e
Felipe Portela <felipelimaportela@gmail.com>
2021-05-22 19:31:04 -0300

    Adicionando e organizando scripts no github

--- Scripts/Query's Hints.sql
+++
@@ -1,6 +0,0 @@
--- QUERY's Hints..
-
--- Ao adicionar esse Hint no final, o SELECT passa a usar o plano de execução do SQL Server 2012, 
-	--ou seja, após migração se um select estiver muito lento, teste.
-	OPTION(QUERYTRACEON 9481) 
-
